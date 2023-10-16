d1932fd2d23087dab7e85db1e6ba4a491df8bc37
DESKTOP-91473CL\B24 <sdbook@gmail.com>
2023-10-16 10:40:01 +0800

    clear db password

--- dbconfig.php
+++ dbconfig.php
@@ -3,8 +3,8 @@
 連線資料庫用的副程式
 */
 $host = 'localhost'; //執行DB Server 的主機
-$user = 'jc'; //登入DB用的DB 帳號
-$pass = '12345'; //登入DB用的DB 密碼
+$user = ''; //登入DB用的DB 帳號
+$pass = ''; //登入DB用的DB 密碼
 $dbName = 'test'; //使用的資料庫名稱
 /* $db 即為未來執行SQL指令所使用的物件 */
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
