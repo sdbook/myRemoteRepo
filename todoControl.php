c5075ce3510fc7296b9d9136f6e964e331c97f17
DESKTOP-91473CL\B24 <sdbook@gmail.com>
2023-10-23 11:36:46 +0800

    addJob now use controller and model

--- todoControl.php
+++ todoControl.php
@@ -7,8 +7,13 @@ case "listJob":
   $jobs=getJobList();
   echo json_encode($jobs);
   return;  
-case "":
-  break;
+case "addJob":
+	$jobName=$_POST['name']; //$_GET, $_REQUEST
+	$jobUrgent=$_POST['urgent'];
+	$jobContent=$_POST['content'];
+	//verify
+	addJob($jobName,$jobUrgent,$jobContent);
+	return;
 default:
   
 }
